ada43b18c80466cad7282a71ae13ca584dade807
kumarsuraj0007 <itssurajkumar0007@gmial.com>
2023-07-15 11:51:50 +0530

    Deleted Oauth

--- app.js
+++ app.js
@@ -1,72 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const sessions = require('express-session');
-const { getName } = require('./date.js');
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
-// express-session and middleware
-app.use(sessions({
-    secret: 'dog',
-    resave: true,
-    saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+// express middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", 'ejs');
-require('./auth.js');
 
-let items = ["Buy Food", "Buy Clothes", "Learn Marketing"];
-let workItems = [];
-
-// function to check weather a user loggedIn or not! 
-function isLoggedIn (req, res, next) {
-    req.user ? next() : res.sendStatus(401)
-}
+mongoose 
+.connect(process.env.MONGO_CONNECTION, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true})   
+.then(() => console.log("Database connected!"))
+.catch(err => console.log(err));
 
 
-// google authentication 
-app.get('/auth/google',
-    passport.authenticate('google', {scope:['email', 'profile']})
-);
-
-app.get('/google/callback', passport.authenticate('google',{
-    successRedirect: '/protected',
-    failureRedirect: '/auth/failure'
-}));
-
-app.get('/auth/failure', (req, res) => {
-    res.send('Something went wrong!')   
+const itemSchema = mongoose.Schema({
+    name: String
 });
+
+const Item = mongoose.model('items', itemSchema);
 
 // requests 
-app.get('/', (req, res) => {
-    res.render('login')
+app.post('/', (req, res) => {
+    var newTodo = req.body.newItem;
+    const newTodoItem = new Item({
+        name:newTodo
+    });
+    newTodoItem.save();
+    res.redirect('/') 
 });
 
-app.get('/protected', isLoggedIn, (req, res) => {
-let day = date.getDate();
-let profileName = `Hello ${req.user.displayName}`;
-    res.render("list", { listTitle: day, newItems: items, listName: profileName });
+app.post('/delete', (req, res) => {
+    const checkedIdItems = req.body.checkbox;
+    Item.findByIdAndDelete(checkedIdItems).then((function(err) {
+        if(!err) {
+            console.log('Successfully Deleted')
+            res.redirect('/')
+        }
+    }))
 });
 
-app.post('/', isLoggedIn, (req, res) => {
-    var item = req.body.newItem;
-    if(req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work")
-    } else {
-        items.push(item);
-        res.redirect('/protected') 
-    }
-});
-
-app.get('/work', (req, res) => {
-    res.render('list', {listTitle: "Work List", newItems: workItems})
+app.get('/', async(req, res)=>{
+    const todoListItems = await Item.find();
+    res.render("list", { listTitle: "Today's List", newItems: todoListItems});
 });
 
 app.listen(4000, () => console.log('Server is running on port 4000!'));
